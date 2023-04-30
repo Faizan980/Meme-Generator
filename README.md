f451da796c5a70545f2728ed6e0ada20138a3c26
Faizan <hashmif747@gmail.com>
2023-04-29 17:45:08 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Meme-Generator
+ Web based Application
---
+++ components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+export default function Header() {
+    return (
+
+        <header className="header">
+            <img 
+                src="../src/assets/Troll Face.svg" 
+                className="header--img"
+            />
+            <h1 className="header--title">Meme Generator</h1>
+            <h4 className="header--project">Fill and Click button - Enjoy</h4>
+        </header>        
+    )
+}
