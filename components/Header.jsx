e0223ec962956b48a44ffa0e4884a7e136f2bc78
Faizan Ashraf <hashmif747@gmail.com>
2023-11-25 02:42:26 +0500

    Added some new elements in the design
    Change the classes for the design in order to update them in the style.css file.

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,14 @@
 import React from "react"
 
-
 export default function Header() {
     return (
-
         <header className="header">
             <img 
-                src="../src/assets/Troll Face.svg" 
-                className="header--img"
+                src="./images/troll-face.png" 
+                className="header--image"
             />
-            <h1 className="header--title">Meme Generator</h1>
-            <h4 className="header--project">Fill and Click button - Enjoy</h4>
-        </header>        
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
     )
 }
