ca0595504af2c71176f7130debd4f4327812d158
Faizan Ashraf <hashmif747@gmail.com>
2023-11-25 02:47:35 +0500

    Change function name
    Updated function name to some meaningful name.

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-export default function Meme() {
+export default function Main() {
     const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
